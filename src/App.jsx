8eec130c048072359166233948785c7b83c0b38b
Barbara-RA <133446746+Barbara-RA@users.noreply.github.com>
2024-07-09 22:04:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Apresentacao from './componentes/Apresentacao';
+import Box from './componentes/Box';
+
+function App() {
+  return (
+    <>  
+    <Apresentacao/>
+    <Box texto='PLANO DE DESENVOLVIMENTO INSTITUCIONAL'/>
+    </> 
+  );
+}
+
+export default App;
